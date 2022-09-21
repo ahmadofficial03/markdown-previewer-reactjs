48190621042bedfe0bbb40b6b4fe454c9d46b0d4
ahmad fraz <ahmadofficial0000@gmail.com>
2022-09-21 22:45:42 +0500

    first commit markdown

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import {marked} from 'marked';
+import './App.css';
+
+
+// a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+function App() {
+  const [text, setText] = useState(placeholder);
+
+  marked.setOptions({
+    breaks: true
+  })
+
+  return (
+    <div className="App">
+      <div className='markdown-editor'>
+        <h2>Editor</h2>
+        <textarea 
+          id='editor'
+          onChange={(event) => {
+          setText(event.target.value);
+          }}
+          value={text}
+        ></textarea>
+      </div>
+
+
+      <div className='markdown-previewer'>
+        <h2>Previewer</h2>
+        <div 
+        id='preview'
+        dangerouslySetInnerHTML={{
+          __html: marked(text),
+        }}
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
